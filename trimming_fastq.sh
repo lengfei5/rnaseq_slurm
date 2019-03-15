e0c7369c0cc65645c50a5aa426f3f6e0b27ab858
jingkui.wang <jingkui.wang@imp.ac.at>
2019-03-15 12:02:36 +0100

    updating trimming_fastq.sh for quant-seq data in ii2

--- trimming_fastq.sh
+++ trimming_fastq.sh
@@ -1,61 +1,69 @@
 #####################################
 # script to trim adaptors for fastq files and run fastqc after trimming
 # two modules possible: cutadapt which need the adaptor sequences to be speficied
-# but the adavantage of using cutadapt isthat it can trim polyA at the same time;
+# but the adavantage of using cutadapt isthat it can trim polyA at the same time (however this polyA trimming is not
+# implemented for some reasons)
 # trimglore which does not need the adaptor sequences and this option is removed now
 #  
 #####################################
-USE_cutadapt="TRUE";
-use_adatpSeq="TRUE"
+adaptor_seq="AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG" # quant-seq adaptor
+#adaptor_seq="CTGTCTCTTATACACATCTCCGAGCCCACGAGAC"; # Nextera adatptor
 trim_polyA="TRUE";
 
-if [ "$USE_cutadapt" == "TRUE" ]; then
-    #adaptor_seq="AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG" # quant-seq adaptor
-    adaptor_seq="CTGTCTCTTATACACATCTCCGAGCCCACGAGAC"; # Nextera adatptor
-    minLength_overlap=3;
-    times_trimming=1;
-    minimumLength=20;
-fi
+minLength_overlap=3;
+times_trimming=1;
+minimumLength=20;
+firstbpToClip=6
 
-CORES=1
 DIR=`pwd`
-
 DIR_input="${DIR}/ngs_raw/FASTQs"
 DIR_trimmed="${DIR}/ngs_raw/FASTQs_trimmed"
-DIR_fastqc="${DIR}/ngs_raw/FASTQCs/"
-#echo $DIR_input
-#echo $DIR_trimmed
-#echo $DIR_output
+DIR_fastqc="${DIR}/ngs_raw/FASTQCs_trimmed/"
 
 mkdir -p $DIR_trimmed
 mkdir -p $DIR_fastqc
 mkdir -p "${DIR}/logs"
 
-
+jobName='trimming'
 for file in ${DIR_input}/*.fastq;
 do
     echo $file
+    fname=`basename ${file%.fastq}`
     trimmed="${file%.fastq}_trimmed.fastq"
-    #echo $trimmed
-    if [ "${USE_cutadapt}" == "TRUE" ]; then
-	cd $DIR_input;
-	out=`basename $trimmed`;
-	#out=${DIR_trimmed}/${out}
-	echo $out
-	qsub -q public.q -o ${DIR}/logs -j yes -pe smp $CORES -cwd -b y -shell y -N trimming "module load fastqc/0.11.5; \
-         module load python; module load cutadapt/1.12.0;  \
-         cutadapt -a $adaptor_seq -n $times_trimming -O $minLength_overlap -m $minimumLength -f fastq -o ${out} $file > ${DIR_trimmed}/${out}.cutadapt.log; \
-         mv ${out} $DIR_trimmed; fastqc ${DIR_trimmed}/${out} -o ${DIR_fastqc}"
-        
-	cd $DIR;
+    echo $fname $trimmed
+    
+    cd $DIR_input;
+    out=`basename $trimmed`;
+    echo $out
+            
+    # creat the script for each sample
+    script=$PWD/${fname}_${jobName}.sh
+    cat <<EOF > $script
+#!/usr/bin/bash
 
-    else
-	qsub -q public.q -o ${DIR}/logs -j yes -pe smp $CORES -cwd -b y -shell y -N trimming " \
-        module load fastqc/0.11.5; \
-        cutadapt/1.12.0;\
-        /home/imp/jingkui.wang/local/bin/trim_galore $file --phred33 \
-        --fastqc --fastqc_args \"--nogroup --outdir ${DIR_fastqc}\" \
-        --nextera --stringency 1 --dont_gzip --length 10 -o $DIR_trimmed;" 
-    fi
+#SBATCH --cpus-per-task=1
+#SBATCH --time=120
+#SBATCH --mem=8000
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o $DIR/logs/$fname.out
+#SBATCH -e $DIR/logs/$fname.err
+#SBATCH --job-name $jobName
+
+module load cutadapt/1.9.1-foss-2017a-python-2.7.13
+module load fastqc/0.11.5-java-1.8.0_121
+
+cutadapt -a $adaptor_seq -u $firstbpToClip \
+-n $times_trimming --overlap $minLength_overlap -m $minimumLength -f fastq \
+-o ${out} $file > ${DIR_trimmed}/${out}.cutadapt.log;
+mv ${out} $DIR_trimmed
+fastqc ${DIR_trimmed}/${out} -o ${DIR_fastqc}
+
+EOF
+
+    cat $script;
+    sbatch $script
+    cd $DIR;
+    
     #break;
 done
