b42d20e79ae259d8cbcc4aa033df9e6131851ca0
jingkui.wang <jingkui.wang@imp.ac.at>
2019-03-15 16:02:59 +0100

    update htseq_counts.sh for c. elegans

--- htseq_counts.sh
+++ htseq_counts.sh
@@ -1,21 +1,28 @@
-#GENOME="/groups/bell/jiwang/Genomes/Mouse/mm10_UCSC/mm10_Refseq_index_4star"
-#GTF="/groups/bell/jiwang/Genomes/Mouse/mm10_UCSC/Sequence/mm10_refGene.gtf"
-GTF="/groups/bell/jiwang/Genomes/C_elegans/WBcel235/Sequence_Annotation/Caenorhabditis_elegans.WBcel235.88.gtf"
-CORES=1
+#####
+# count reads with HTSeq
+#####
+# this gtf has all annotation including non-coding regions and miRNAs
+#GTF="/groups/bell/jiwang/Genomes/C_elegans/WBcel235/Sequence_Annotation/Caenorhabditis_elegans.WBcel235.88.gtf"
+# only protein coding genes
+GTF='/groups/cochella/jiwang/annotations/Caenorhabditis_elegans.WBcel235.88_proteinCodingGenes.gtf'
 
+nb_cores=1
+jobName='htseq'
 format=bam
-strandSpec="no";
+strandSpec=yes;
 cutoff_quality=10
 mode="intersection-nonempty"
 ID_feature="gene_id"
 
 DIR=`pwd`
 DIR_input="${DIR}/BAMs"
-DIR_output="${DIR}/readCounts_htseq"
+DIR_output="${DIR}/htseq_counts"
+dir_logs=$PWD/logs
 echo $DIR_input
 echo $DIR_output
+
 mkdir -p $DIR_output;
-mkdir -p $PWD/logs
+mkdir -p $dir_logs
 
 for file in ${DIR_input}/*.bam;
 do
@@ -24,6 +31,28 @@ do
     #echo $fname
     file_output=${fname%.bam}.txt
     #echo $file_output
-    qsub -q public.q -o ${PWD}/logs -j yes -pe smp $CORES -cwd -b y -shell y -N HTseq_counts "module load htseq-count/0.6.1; htseq-count -f $format -s $strandSpec -a $cutoff_quality -t exon -i $ID_feature -m $mode -f bam $file $GTF > ${DIR_output}/$file_output; module load samtools; samtools index $file "
+
+    script=${dir_logs}/$(basename $file)_${jobName}.sh
+    cat <<EOF > $script
+#!/usr/bin/bash
+
+#SBATCH --cpus-per-task=$nb_cores
+#SBATCH --time=120
+#SBATCH --mem=6000
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o ${dir_logs}/$(basename $file).out
+#SBATCH -e ${dir_logs}/$(basename $file).err
+#SBATCH --job-name $jobName
+
+module load htseq/0.9.1-foss-2017a-python-2.7.13
+htseq-count -f $format -s $strandSpec -a $cutoff_quality -t exon \
+-i $ID_feature -m $mode -f bam $file $GTF \
+> ${DIR_output}/$file_output
+
+EOF
+
+    cat $script;  
+    
     #break;
 done
