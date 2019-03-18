158d8f3b54a38e261dd8a29da1abd4daf2f89add
jingkui.wang <jingkui.wang@imp.ac.at>
2019-03-18 14:43:48 +0100

    add new counting function with featurecount

---
+++ featureCounts.sh
@@ -0,0 +1,62 @@
+#####
+# count reads with HTSeq
+#####
+# this gtf has all annotation including non-coding regions and miRNAs
+GTF="/groups/bell/jiwang/Genomes/C_elegans/WBcel235/Sequence_Annotation/Caenorhabditis_elegans.WBcel235.88.gtf"
+# only protein coding genes
+#GTF='/groups/cochella/jiwang/annotations/Caenorhabditis_elegans.WBcel235.88_proteinCodingGenes.gtf'
+
+nb_cores=1
+jobName='featurecounts'
+format=bam
+strandSpec=1;
+cutoff_quality=10
+#mode="intersection-nonempty"
+#ID_feature="gene_id"
+
+DIR=`pwd`
+DIR_input="${DIR}/BAMs"
+DIR_output="${DIR}/featurecounts"
+dir_logs=$PWD/logs
+echo $DIR_input
+echo $DIR_output
+
+mkdir -p $DIR_output;
+mkdir -p $dir_logs
+
+for file in ${DIR_input}/*.bam;
+do
+    echo $file
+    fname="$(basename $file)"
+    #echo $fname
+    file_output=${fname%.bam}
+    #echo $file_output
+
+    script=${dir_logs}/$(basename $file)_${jobName}.sh
+    cat <<EOF > $script
+#!/usr/bin/bash
+
+#SBATCH --cpus-per-task=$nb_cores
+#SBATCH --time=120
+#SBATCH --mem=6000
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o ${dir_logs}/$(basename $file).out
+#SBATCH -e ${dir_logs}/$(basename $file).err
+#SBATCH --job-name $jobName
+
+module load subread/1.5.0-p1-foss-2017a
+
+featureCounts -t exon -a $GTF -Q $cutoff_quality -g gene_id \
+-o ${DIR_output}/${file_output}_gene.featureCounts.txt \
+-s $strandSpec $file; \
+featureCounts -t exon -a $GTF -Q $cutoff_quality -g gene_biotype \
+-o ${DIR_output}/${file_output}_biotype.featureCounts.txt \
+-s $strandSpec $file 
+
+EOF
+
+    cat $script;  
+    sbatch $script
+    #break;
+done
