7818c8331433f905464218232e2da3012b313520
jingkui.wang <jingkui.wang@imp.ac.at>
2019-08-01 10:36:58 +0200

    add new script for umitools and runSlamdunk, both for quant-seq data

---
+++ runSlamdunk_ii2.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+#SBATCH --cpus-per-task=20
+#SBATCH --time=8:00:00
+#SBATCH --mem=30G
+
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o slamseq.out
+#SBATCH -e slamseq.err
+#SBATCH --job-name slamseq
+
+OUT=$PWD/slamdunk_R7799_trimmed_ii2
+
+# input files
+files=/groups/bell/jiwang/Projects/Jorge/NGS_requests/R7799_slamseq/ngs_raw/FASTQs_trimmed/CDMYJANXX_1_87750_CGATCTrandom_trimmed.fastq 
+
+REF=/groups/cochella/jiwang/scripts/slamseq/reference/mm10/genome.fa
+FILTERBED=/groups/cochella/jiwang/scripts/slamseq/annotation/mm10_Annotation_BurkardT/170614custom_mESCannotation_mapping_Thomas.bed
+BED=/groups/cochella/jiwang/scripts/slamseq/annotation/mm10_Annotation_BurkardT/170614custom_mESCannotation_counting_Thomas.bed
+
+# echo $files
+# parameters
+RL=100
+BQ=27
+T=20
+
+hostname
+
+module load gcc/6.3.0-2.27
+source activate slamdunk
+
+slamdunk all -b $BED -fb $FILTERBED -r $REF -o $OUT -5 12 -t $T -rl $RL -n 100 -m -mbq $BQ -q --skip-sam ${files}
+# alleyoop summary -o $OUT/summary.txt -t $OUT/count $OUT/filter/${files/.fastq/}*bam
---
+++ umitools_dedup.sh
@@ -0,0 +1,44 @@
+###################
+## deduplicate reads integrated with umi uisng umi-tools
+###################
+jobName="umi_dedup"
+DIR_INPUT="${PWD}/BAMs"
+DIR_OUT="${PWD}/BAMs_umi"
+dir_logs=$PWD/logs
+
+mkdir -p ${DIR_OUT}
+mkdir -p ${dir_logs}
+
+for file in $DIR_INPUT/*.bam;
+do 
+    FILENAME="$(basename $file)";
+    fname=${FILENAME%.bam};
+    fname=${fname/\#/\_}
+    echo "$file" 
+    echo $fname
+    
+    ## creat the script for each sample 
+    script=${dir_logs}/${fname}_${jobName}.sh
+    cat <<EOF > $script
+#!/usr/bin/bash
+
+#SBATCH --cpus-per-task=1 
+#SBATCH --time=90 
+#SBATCH --mem=10000 
+
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o ${dir_logs}/${fname}.out 
+#SBATCH -e ${dir_logs}/${fname}.err 
+#SBATCH --job-name ${jobName}
+
+source activate umitools
+umi_tools dedup --stdin=$file --log=${dir_logs}/${fname}_${jobName}.log --stdout ${DIR_OUT}/${fname}_umiDedup.bam
+
+EOF
+    
+    cat $script;
+    sbatch $script
+    break;
+    
+done
