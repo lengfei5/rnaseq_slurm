6513a4957580e20efa445895b1b10898ac0642b5
jingkui.wang <jingkui.wang@imp.ac.at>
2020-02-17 10:47:49 +0100

    add parameters for protein coding GTF and non-coding GTF

--- htseq_counts.sh
+++ htseq_counts.sh
@@ -2,13 +2,14 @@
 # Count reads of RNA-seq data with HTSeq
 # this gtf used here has only protein coding genes, which potentially keep more reads for quant-seq data
 ############################
-while getopts ":hD:s:q:m:" opts; do
+while getopts ":hG:D:s:q:m:" opts; do
     case "$opts" in
         "h")
             echo "script to count reads for RNA-seq using htseq"
             echo "available genomes: WBcel235"
             echo "Usage: "
             echo "-h help"
+	    echo "-G gtf file for gene annotation"
             echo "-D directory of bam files (by default $PWD/BAMs)"
             echo "-s strand-specifity (no, yes, reverse)"
 	    echo "-q threshold of read quality (default value 10)"
@@ -23,6 +24,9 @@ while getopts ":hD:s:q:m:" opts; do
         "D")
             DIR_input="$OPTARG"
             ;;
+	"G")
+	    GTF="$OPTARG"
+	    ::
         "s")
             strandSpec="$OPTARG"
             ;;
@@ -48,6 +52,15 @@ format=bam
 DIR=`pwd`
 
 if [ -z "$DIR_input" ]; then DIR_input="$PWD/BAMs"; fi; # bam directory
+
+# GTF file
+# protein-coding and non-coding (default)
+if [ -z "$GTF" ]; then
+    GTF="/groups/cochella/jiwang/annotations/Caenorhabditis_elegans.WBcel235.88.gtf"; 
+fi;
+# only protein coding genes
+# GTF='/groups/cochella/jiwang/annotations/Caenorhabditis_elegans.WBcel235.88_proteinCodingGenes.gtf'# protein-coding and non-coding
+
 if [ -z "$strandSpec" ]; then strandSpec=yes; fi # specificity
 if [ -z "$cutoff_quality" ]; then cutoff_quality=10; fi; # cutoff of read quality
 if [ -z "$mode" ]; then mode="intersection-nonempty"; fi;
@@ -57,10 +70,6 @@ DIR_output="${DIR}/htseq_counts_$(basename $DIR_input)"
 dir_logs=$PWD/logs
 echo "input folder -- " $DIR_input
 echo "output folder -- "$DIR_output
-
-# only protein coding genes
-#GTF="/groups/bell/jiwang/Genomes/C_elegans/WBcel235/Sequence_Annotation/Caenorhabditis_elegans.WBcel235.88.gtf"
-GTF='/groups/cochella/jiwang/annotations/Caenorhabditis_elegans.WBcel235.88_proteinCodingGenes.gtf'
 
 mkdir -p $DIR_output;
 mkdir -p $dir_logs
