dd29a45daf41e609c2cfd1afaa7896b615448938
jingkui.wang <jingkui.wang@imp.ac.at>
2021-02-01 15:18:31 +0100

    update index_genome_star.sh

--- index_genome_star.sh
+++ index_genome_star.sh
@@ -3,8 +3,8 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --qos=medium
 #SBATCH --time=2-00:00:00
-#SBATCH --partition=c
-#SBATCH --mem=150G
+#SBATCH --partition=m
+#SBATCH --mem=500G
 
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
@@ -15,8 +15,8 @@
 module load star/2.5.2a-foss-2018b
 
 Genome='/groups/tanaka/People/current/jiwang/Genomes/axolotl/AmexG_v6.DD.corrected.round2.chr.fa'
-GTF='/groups/tanaka/People/current/jiwang/Genomes/axolotl/AmexT_v47.release.gtf'
+GTF='/groups/tanaka/People/current/jiwang/Genomes/axolotl/annotations/AmexT_v47.release.gtf'
 OUT=${PWD}/ax6
 mkdir -p ${PWD}/ax6
 
-STAR --runMode genomeGenerate --runThreadN 32 --genomeDir $OUT --genomeFastaFiles $Genome --sjdbGTFfile $GTF --limitGenomeGenerateRAM 161061273600 --genomeSAsparseD 2
+STAR --runMode genomeGenerate --runThreadN 32 --genomeDir $OUT --genomeFastaFiles $Genome --sjdbGTFfile $GTF --limitGenomeGenerateRAM 429496729600 --genomeSAsparseD 1
