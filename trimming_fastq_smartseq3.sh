05bac3de878deade7675d5cb0d94bc093cae74ef
jingkui.wang <jingkui.wang@imp.ac.at>
2022-11-17 09:31:36 +0100

    add script for smartseq3

---
+++ trimming_fastq_smartseq3.sh
@@ -0,0 +1,74 @@
+#####################################
+# script to trim adaptors for fastq files and run fastqc after trimming
+# two modules possible: cutadapt which need the adaptor sequences to be speficied
+# but the adavantage of using cutadapt isthat it can trim polyA at the same time (however this polyA trimming is not
+# implemented for some reasons)
+# trimglore which does not need the adaptor sequences and this option is removed now
+#
+# add polyA trimming in cutadapt
+#####################################
+adaptor_seq="AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG" # quant-seq adaptor
+firstbpToClip=6 # nb of first bps were clipped for quant-seq
+
+#adaptor_seq="A{10}"
+trim_polyA="TRUE";
+
+minLength_overlap=3; # default 3 
+times_trimming=1;# default 1 
+minimumLength=20; # default 0
+
+
+DIR=`pwd`
+DIR_input="${DIR}/ngs_raw/FASTQs"
+DIR_trimmed="${DIR}/ngs_raw/FASTQs_trimmed"
+DIR_fastqc="${DIR}/ngs_raw/FASTQCs_trimmed/"
+
+mkdir -p $DIR_trimmed
+mkdir -p $DIR_fastqc
+mkdir -p "${DIR}/logs"
+
+jobName='trimming'
+for file in ${DIR_input}/*.fastq;
+do
+    echo $file
+    fname=`basename ${file%.fastq}`
+    trimmed="${file%.fastq}_trimmed.fastq"
+    echo $fname $trimmed
+    
+    cd $DIR_input;
+    out=`basename $trimmed`;
+    echo $out
+    
+    # creat the script for each sample
+    script=$PWD/${fname}_${jobName}.sh
+    cat <<EOF > $script
+#!/usr/bin/bash
+
+#SBATCH --cpus-per-task=1
+#SBATCH --time=120
+#SBATCH --mem=8G
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o $DIR/logs/$fname.out
+#SBATCH -e $DIR/logs/$fname.err
+#SBATCH --job-name $jobName
+
+module load cutadapt/1.18-foss-2018b-python-3.6.6
+#module load fastqc/0.11.8-java-1.8
+
+cutadapt -a $adaptor_seq -u $firstbpToClip \
+-n $times_trimming --overlap $minLength_overlap -m $minimumLength -f fastq \
+-o ${out} $file > ${DIR_trimmed}/${out}.cutadapt.log;
+mv ${out} $DIR_trimmed
+
+#fastqc ${DIR_trimmed}/${out} -o ${DIR_fastqc}
+
+EOF
+
+    cat $script;
+    sbatch $script
+    cd $DIR;
+    
+    #break;
+    
+done
---
+++ umitools_extract_smartseq3_PE.sh
@@ -0,0 +1,61 @@
+###################
+# umi extract with umi_tools extract for PE
+# there is an issue for PE https://github.com/CGATOxford/UMI-tools/issues/391
+# if umi is in the R2, R2 should be go first
+# new version of umi-tools should fix this, but cbe currently does not have it
+###################
+DIR_INPUT="${PWD}/ngs_raw/FASTQs"
+DIR_OUT="${PWD}/ngs_raw/FASTQs_umi"
+dir_logs=$PWD/logs
+#echo ${DIR_OUT};
+
+mkdir -p ${DIR_OUT}
+
+mkdir -p ${dir_logs}
+
+for seq1 in `ls ${DIR_INPUT}/*.fastq | grep "_R1"`;
+do
+    fname="$(basename $seq1)"
+    SUFFIX=${fname#*_R1}
+    fname=${fname%_R1*}
+    
+    seq2=${DIR_INPUT}/${fname}_R2${SUFFIX};
+    echo $fname
+    echo $seq1
+    echo $seq2
+    
+    ## creat the script for each sample 
+    script=${dir_logs}/${fname}_umi_extract.sh
+    cat <<EOF > $script
+#!/usr/bin/bash
+
+#SBATCH --cpus-per-task=1 
+#SBATCH --time=180 
+#SBATCH --mem=2G
+
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH -o ${dir_logs}/${fname}.out 
+#SBATCH -e ${dir_logs}/${fname}.err 
+#SBATCH --job-name umi_extract
+
+#ml load umi-tools/1.0.0-foss-2018b-python-3.6.6
+ml purge
+ml build-env/f2021
+ml umi-tools/1.1.2-foss-2020b-python-3.8.6
+
+umi_tools extract \
+-I $seq1 \
+-S ${DIR_OUT}/${fname}_umiExtracted_R1.fastq \
+--extract-method regex \
+--bc-pattern='^(?P<discard_1>.{16})(?P<umi_1>.{10})(?P<discard_2>TTTCTTATATGGG).*' \
+--read2-in=$seq2 \
+--read2-out=${DIR_OUT}/${fname}_umiExtracted_R2.fastq
+
+EOF
+    
+    cat $script;
+    sbatch $script
+    #break;
+    
+done
